af44fea435bd44f283a25ee48a920588936a538b
ZekeMarshall <marshall_zeke@outlook.com>
2025-10-17 13:16:17 +0100

    Generating pseudo-quadrats

---
+++ data-raw/create_constants.R
@@ -0,0 +1,2 @@
+mnnpc_vc_types <- list("APn8", "APn9", "FD", "FF", "FP", "FPn73", "MH", "MR", "OP", "RO", "UP", "WF", "WFn74", "WM", "WP")
+usethis::use_data(mnnpc_vc_types, internal = FALSE, overwrite = TRUE)
