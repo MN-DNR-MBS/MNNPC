90503ebbbbb930b4e11d63bdf7f6a110ce28bc39
ZekeMarshall <marshall_zeke@outlook.com>
2025-11-07 11:36:54 +0000

    Adding internal data, just example_data_options at present

--- data-raw/create_constants.R
+++ data-raw/create_constants.R
@@ -2,6 +2,9 @@
 
 # mnnpc_taxa_aggregation <- mnnpc_taxonomic_backbone 
 
+example_data_options <- c("None" = "none", "St. Croix State Forest" = "St. Croix State Forest")
+usethis::use_data(example_data_options, internal = TRUE, overwrite = TRUE)
+
 mnnpc_vc_types <- list("AP", "FD", "FF", "FP", "MH", "MR", "OP", "RO", "UP", "WF", "WM", "WP")
 usethis::use_data(mnnpc_vc_types, internal = FALSE, overwrite = TRUE)
 
