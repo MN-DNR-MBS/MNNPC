8bfe35955e267272d3fb745812ced645e59bfa9a
ZekeMarshall <marshall_zeke@outlook.com>
2025-11-07 14:38:07 +0000

    Adding names to mnnpc_vc_types constant

--- data-raw/create_constants.R
+++ data-raw/create_constants.R
@@ -6,5 +6,5 @@ example_data_options <- c("None" = "none", "St. Croix State Forest" = "St. Croix
 usethis::use_data(example_data_options, internal = TRUE, overwrite = TRUE)
 
 mnnpc_vc_types <- list("AP", "FD", "FF", "FP", "MH", "MR", "OP", "RO", "UP", "WF", "WM", "WP")
+names(mnnpc_vc_types) <- mnnpc_vc_types
 usethis::use_data(mnnpc_vc_types, internal = FALSE, overwrite = TRUE)
-
