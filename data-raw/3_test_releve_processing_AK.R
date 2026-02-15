97957dfea10289e741acba45791aedafd34aa4b2
Amy Kendig <aekendig@gmail.com>
2026-02-15 17:32:22 -0600

    update releve testing

--- data-raw/3_test_releve_processing_AK.R
+++ data-raw/3_test_releve_processing_AK.R
@@ -4,7 +4,6 @@
 load("data-raw/data-out-ak/releve_species_grouped_data.rds")
 load("data-raw/data-out-ak/releve_species_ungrouped_data.rds")
 load("data-raw/data-out-ak/releve_plot_data.rds")
-load("../npc-releve/data/originals-20260208/crosswalk.rds")
 
 # remove everything except above
 rm(list = ls()[!ls() %in% c("crosswalk",
@@ -44,16 +43,16 @@ parms <- data.frame(relnumb = rels,
 
 # select releve to test
 # add columns
-dat_raw <- crosswalk |>
-  dplyr::select(relnumb, physcode, minht, maxht, pcov, taxon, relid, scov,
+dat_raw <- releve_species_ungrouped |>
+  dplyr::select(relnumb, physcode, minht, maxht, taxon, scov,
                 outside_of_plot) |>
   dplyr::inner_join(parms) |>
-  dplyr::rename(scov = scov)
+  dplyr::arrange(year, group, relnumb, taxon, minht, maxht)
 
 
 #### process data ####
 
-dat_proc <- process_dnr_releves(dat_raw, cover_scale = "braunBlanquet")
+dat_proc <- process_dnr_releves(dat_raw, cover_scale = "percentage")
 
 
 #### pre-processed data ####
@@ -82,62 +81,31 @@ dat_val[rows_mis, ]
 #### example data ####
 
 # process datasets
-dat_ex_proc1 <- process_dnr_releves(mnnpc_example_data[[1]])
-dat_ex_proc2 <- process_dnr_releves(mnnpc_example_data[[2]])
+dat_ex_proc1 <- process_dnr_releves(mnnpc_example_data[[1]] |>
+                                      dplyr::filter(year == 1), 
+                                    cover_scale = "braunBlanquet")
+dat_ex_proc2 <- process_dnr_releves(mnnpc_example_data[[2]] |>
+                                      dplyr::filter(year == 1), 
+                                    cover_scale = "braunBlanquet")
 
-# format resample data
-# format resample data
-resample2 <- resample |>
-  dplyr::filter(stringr::str_detect(npc, "MHs38|MHs39")) |> 
-  dplyr::select(-relnumb) |>
-  dplyr::rename(relnumb = orig_relnumb) |> 
-  dplyr::full_join(resample |>
-                     dplyr::filter(stringr::str_detect(npc, "MHs38|MHs39"))) |> 
-  dplyr::mutate(group = ifelse(stringr::str_detect(npc, "MHs38"), 
-                               "Oak-Basswood Group", 
-                               "Maple-Basswood Group"))
-
-# get releve numbers in the database
-dat_ex_relnumb1 <- releve |>
-  dplyr::filter(relnumb %in% dat_ex_proc1$Quadrat |
-                  original_releve_nbr %in% dat_ex_proc1$Quadrat) |>
-  dplyr::mutate(year = lubridate::year(date_),
-                group = dplyr::case_when(is.na(place_name) ~ "Control",
-                                         stringr::str_detect(place_name, 
-                                                    "Outside Exclosure") ~ 
-                                           "Outside Exclosure",
-                                         stringr::str_detect(place_name, "Exclosure") ~ 
-                                           "Inside Exclosure"),
-                orig_relnumb = ifelse(relnumb %in% dat_ex_proc1$Quadrat,
-                                      relnumb, original_releve_nbr)) |>
-  dplyr::select(year, group, orig_relnumb, relnumb)
-
-dat_ex_relnumb2 <- releve |>
-  dplyr::filter(relnumb %in% dat_ex_proc2$Quadrat |
-                  original_releve_nbr %in% dat_ex_proc2$Quadrat) |>
-  dplyr::left_join(resample2 |>
-                     dplyr::select(relnumb, group)) |>
-  dplyr::mutate(year = lubridate::year(date_),
-                year = ifelse(year < 2018, 1990, year),
-                orig_relnumb = ifelse(relnumb %in% dat_ex_proc2$Quadrat,
-                                      relnumb, original_releve_nbr)) |>
-  dplyr::select(year, group, orig_relnumb, relnumb)
-  
 # previously processed example data
-dat_ex_val1 <- crosswalk_pre |>
-  dplyr::inner_join(dat_ex_relnumb1) |>
-  dplyr::select(year, group, orig_relnumb, analysis_group_strata, scov_adj) |>
-  dplyr::arrange(year, group, orig_relnumb, analysis_group_strata)|>
-  dplyr::rename(Year = year, Group = group, Quadrat = orig_relnumb,
-                Species = analysis_group_strata, Cover = scov_adj)
+dat_ex_val1 <- releve_species_grouped |>
+  dplyr::inner_join(mnnpc_example_data[[1]] |>
+                      dplyr::filter(year == 1) |>
+                      dplyr::distinct(year, group, relnumb)) |>
+  dplyr::select(year, group, relnumb, analysis_group_strata, scov) |>
+  dplyr::arrange(year, group, relnumb, analysis_group_strata)|>
+  dplyr::rename(Year = year, Group = group, Quadrat = relnumb,
+                Species = analysis_group_strata, Cover = scov)
 
-dat_ex_val2 <- crosswalk_pre |>
-  dplyr::inner_join(dat_ex_relnumb2) |>
-  dplyr::select(year, group, orig_relnumb, analysis_group_strata, scov_adj) |>
-  dplyr::arrange(year, group, orig_relnumb, analysis_group_strata)|>
-  dplyr::rename(Year = year, Group = group, Quadrat = orig_relnumb,
-                Species = analysis_group_strata, Cover = scov_adj)
-
+dat_ex_val2 <- releve_species_grouped |>
+  dplyr::inner_join(mnnpc_example_data[[2]] |>
+                      dplyr::filter(year == 1) |>
+                      dplyr::distinct(year, group, relnumb)) |>
+  dplyr::select(year, group, relnumb, analysis_group_strata, scov) |>
+  dplyr::arrange(year, group, relnumb, analysis_group_strata)|>
+  dplyr::rename(Year = year, Group = group, Quadrat = relnumb,
+                Species = analysis_group_strata, Cover = scov)
 # rows with mismatches
 rows_mis1 <- which(rowSums(dat_ex_proc1 != dat_ex_val1) > 0)
 dat_ex_proc1[rows_mis1, ]
